fb648ba3b0c8c70b84af0ecddfbd1761b8084e0c
alex <alex.sbox24@gmail.com>
2020-03-08 07:43:26 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -37,10 +37,53 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode1(expr) {
+    let string = "";
+    for (let i = 0; i < expr.length; i+=10){
+        let letter = "";
+        let character = expr.slice(i, i + 10);
+        if (character === "**********"){
+            string = string + " ";
+        }else{
+            for (let j = 0; j < character.length; j+=2){
+                let cr = character.slice(j, j+2);
+                if (cr === "10"){
+                    letter = letter + ".";
+                }else if (cr === "11"){
+                    letter = letter + "-";
+                }
+            }
+            string = string + MORSE_TABLE[letter];
+        }
+    }
+    return string;
 }
 
 module.exports = {
     decode
 }
+
+function decode(expr) {
+    let string = "";
+    for (let i = 0; i < expr.length; i+=10){
+        let letter = "";
+        let character = expr.slice(i, i + 10);
+        if (character === "**********"){
+            string = string + " ";
+        }else{
+            for (let j = 0; j < character.length; j+=2){
+                let cr = character.slice(j, j+2);
+                if (cr === "10"){
+                    letter = letter + ".";
+                }else if (cr === "11"){
+                    letter = letter + "-";
+                }
+            }
+            string = string + MORSE_TABLE[letter];
+        }
+    }
+    return string;
+
+}
+// "0010101010 0000000010 0010111010 0010111010 0000111111**********00001011110000111111000010111000101110100000111010"
+//console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
